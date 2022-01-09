effcfcd160280fef9b4488611c9992f99fe3b2b0
Modulariz <agustin.favo@gmail.com>
2022-01-08 22:05:51 -0300

    fix disconnect issue

--- main.go
+++ main.go
@@ -45,6 +45,7 @@ var upgrader = websocket.Upgrader{
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Use /ws route to subscribe my websockets")
 }
+
 func reader(conn *websocket.Conn) {
 	if conn != nil {
 		for {
@@ -58,21 +59,45 @@ func reader(conn *websocket.Conn) {
 				if elem.connType == "listener" {
 					if err := elem.conn.WriteMessage(messageType, p); err != nil {
 						log.Println(err)
-						return
 					}
 				}
 			}
 		}
 	}
 }
+
+func indexOfConn(element connection, data []connection) int {
+	for k, v := range data {
+		if element == v {
+			return k
+		}
+	}
+	return -1
+}
+
+func removeConn(s []connection, i int) []connection {
+	s[i] = s[len(s)-1]
+	return s[:len(s)-1]
+}
+
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 	connType := r.URL.Query().Get("t")
 	ws, err := upgrader.Upgrade(w, r, nil)
+	conn := connection{connType: connType, conn: ws}
+
+	ws.SetCloseHandler(func(code int, text string) error {
+		fmt.Println(text)
+		indexOfConnection := indexOfConn(conn, connections)
+		connections = removeConn(connections, indexOfConnection)
+		var returned error
+		return returned
+	})
+
 	if err != nil {
 		log.Println(err)
 	} else {
 		log.Println("Client Succesfully Connected")
-		connections = append(connections, connection{connType: connType, conn: ws})
+		connections = append(connections, conn)
 		reader(ws)
 	}
 }
@@ -94,8 +119,6 @@ func main() {
 	SECRET = os.Getenv("SECRET")
 	fmt.Println("Go WebSockets")
 	setupRoutes()
-	go func() {
-		log.Fatal(http.ListenAndServe(":80", nil))
-	}()
+	//log.Fatal(http.ListenAndServe(":80", nil))
 	log.Fatal(http.ListenAndServeTLS(":443", "/etc/letsencrypt/live/modularizar.com/fullchain.pem", "/etc/letsencrypt/live/modularizar.com/privkey.pem", nil))
 }
